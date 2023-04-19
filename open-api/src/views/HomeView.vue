75a18b007f25ff8090a2462ba0bc01cc7ed06b45
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-04-19 13:13:25 -0400

    stuff

---
+++ open-api/src/views/HomeView.vue
@@ -0,0 +1,14 @@
+<template>
+  <div></div>
+</template>
+
+<script setup>
+import { ref, onMounted } from 'vue'
+const squirrel = ref(``)
+async function getSquirrels() {
+  let res = await fetch(`https://data.cityofnewyork.us/resource/vfnx-vebw.json`)
+  let data = await res.json()
+}
+</script>
+
+<style lang="scss" scoped></style>
