82e849e2de63418627ff63b684f9f712cde12fde
AdamLyun <adamlyun@gmail.com>
2023-04-19 13:15:03 -0400

    x

---
+++ open-ai/src/views/HomeView.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+
+  </div>
+</template>
+
+<script setup>
+import { ref. onMounted } from "vue";
+const squirrel= ref('') 
+async function getSquirrel() {
+  let res= await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+  let data = await res.json()
+  squirrel.value = data.results  
+}
+onMounted(()=>{
+  :getSquirrel()
+})
+</script>
+
+<style lang="scss" scoped>
+
+</style>
