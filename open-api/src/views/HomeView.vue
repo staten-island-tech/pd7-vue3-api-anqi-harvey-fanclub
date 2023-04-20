dd3a909fa72c92c8eab1b1c7ab2d1cf388f4124a
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-04-20 13:18:44 -0400

    ayy array

--- open-api/src/views/HomeView.vue
+++ open-api/src/views/HomeView.vue
@@ -4,11 +4,17 @@
 
 <script setup>
 import { ref, onMounted } from 'vue'
-const squirrel = ref(``)
+const squirrels = ref('')
 async function getSquirrels() {
   let res = await fetch(`https://data.cityofnewyork.us/resource/vfnx-vebw.json`)
+  console.log(res)
   let data = await res.json()
+  console.log(data)
+  squirrels.value = data
 }
+onMounted(() => {
+  getSquirrels()
+})
 </script>
 
 <style lang="scss" scoped></style>
