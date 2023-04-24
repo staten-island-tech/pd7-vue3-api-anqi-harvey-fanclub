b3333711791f3dc32d355e7adbe8770bf7bb7572
AdamLyun <adamlyun@gmail.com>
2023-04-24 13:15:13 -0400

    eeeeeeeeeeeeeeeeeeee

---
+++ open-ai/src/components/squirrelCard.vue
@@ -0,0 +1,15 @@
+<template>
+  <div class="card">
+    <h2>{{ hectare }}</h2>
+    <h3>{{ unique_squirrel_id }}</h3>
+  </div>
+</template>
+
+<script setup>
+const props = defineProps({
+  hectare: String,
+  unique_squirrel_id: Number,
+});
+</script>
+
+<style lang="scss" scoped></style>
--- open-ai/src/views/HomeView.vue
+++ open-ai/src/views/HomeView.vue
@@ -1,20 +1,26 @@
 <template>
   <div>
-
+ <squirrelCard v-for="(monster, index) in squirrel" :key="monster.hectare" :unique_squirrel_id="monster.unique_squirrel_id" :squirrel="monster">
+ </squirrelCard>
   </div>
 </template>
 
 <script setup>
-import { ref. onMounted } from "vue";
+import { ref, onMounted } from "vue";
+import squirrelCard from "../components/squirrelCard.vue"
 const squirrel= ref('') 
 async function getSquirrel() {
-  let res= await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+  let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
   let data = await res.json()
-  squirrel.value = data.results  
+  squirrel.value = data 
+   console.log(squirrel.value )
 }
 onMounted(()=>{
-  :getSquirrel()
+  getSquirrel()
+  
 })
+
+
 </script>
 
 <style lang="scss" scoped>
