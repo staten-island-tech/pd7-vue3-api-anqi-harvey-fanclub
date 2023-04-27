fb5da42c67b64d6e7a1aec4d157d4089e366a3c1
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-04-26 21:41:44 -0400

    god i fucking hate vue

--- open-api/src/components/DoughnutChart.vue
+++ open-api/src/components/DoughnutChart.vue
@@ -1,6 +1,6 @@
 <template>
-  <div>
-    <Doughnut id="DoughnutChart" v-if="loaded" :data="chartData" />
+  <div class="chart">
+    <Doughnut v-if="loaded" :data="chartData" :options="chartOptions" />
   </div>
 </template>
 
@@ -12,17 +12,33 @@ ChartJS.register(ArcElement, Tooltip, Legend)
 
 export default {
   name: 'DoughnutChart',
-  components: { Doughnut },
-  props: {
-    type: Object,
-    required: true,
-    default: () => ({
-      responsive: true
-    }),
-    chartData: {
-      type: Object,
-      required: true
+  components: {
+    Doughnut
+  },
+  data() {
+    return {
+      loaded: false,
+      chartData: {
+        labels: ['Gray', 'Cinnamon', 'Black'],
+        data: [],
+        backgroundColor: ['#7B888E', '#9c4722', '#3b444b']
+      },
+      chartOptions: {
+        responsive: true,
+        maintainAspectRatio: true
+      }
     }
+  },
+  async mounted() {
+    try {
+      const res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+      const squirrelData = await res.json()
+      squirrelData.forEach((squirrel) => this.chartData.data.push(squirrel.primary_fur_color))
+      this.loaded = true
+    } catch (e) {
+      console.error(e)
+    }
+    console.log(this.chartData)
   }
 }
 </script>
--- open-api/src/views/DoughnutView.vue
+++ open-api/src/views/DoughnutView.vue
@@ -1,37 +1,13 @@
 <template>
-  <div class="chart">
-    <DoughnutChart v-if="loaded" :data="chartData" :options="chartOptions" />
-  </div>
+  <h2>Squirrel Primary Colors</h2>
+  <DoughnutChart />
 </template>
 
 <script>
 import DoughnutChart from '../components/DoughnutChart.vue'
+
 export default {
-  name: 'DoughnutView',
-  components: {
-    DoughnutChart
-  },
-  data() {
-    return {
-      loaded: false,
-      chartData: {
-        labels: [],
-        datasets: []
-      },
-      chartOptions: {}
-    }
-  },
-  async mounted() {
-    try {
-      const res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
-      const squirrelData = res.json()
-      console.log(squirrelData)
-      let labels = ['Gray', 'Cinnamon', 'Black']
-    } catch {}
-  }
+  name: 'App',
+  components: { DoughnutChart }
 }
 </script>
-
-<script setup></script>
-
-<style lang="scss" scoped></style>
