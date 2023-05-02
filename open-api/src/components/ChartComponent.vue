740b76a7247a3ecbe625e1d01715481c55101e1c
AdamLyun <113201169+AdamLyun@users.noreply.github.com>
2023-05-01 21:40:36 -0400

    Merge branch 'harvey's-#1-fan'

--- open-api/src/components/ChartComponent.vue
+++ open-api/src/components/ChartComponent.vue
@@ -1,14 +1,11 @@
 <template>
-  <header>
-    <div id="routing">
-      <RouterLink to="/">Back to Main</RouterLink>
-      <br />
-      <RouterLink to="/doughnut">To Doughnut</RouterLink>
-    </div>
-  </header>
-  <br />
   <Bar v-if="showGraph" :data="data" :options="options" />
-  <div id="button"><button @click="showGraph = true">Click me to show the message</button></div>
+  <Bar v-if="showGraphs" :data="data2" :options="options" />
+  <button @click="createGraph">Create Graph</button>
+  <select type="input" id="input" >
+    <option>Age</option>
+    <option>Primary Fur Color</option>
+  </select>
 </template>
 
 <script>
@@ -19,56 +16,91 @@ import {
   Legend,
   BarElement,
   CategoryScale,
-  LinearScale
-} from 'chart.js'
-import { Bar } from 'vue-chartjs'
+  LinearScale,
+} from "chart.js";
+import { Bar } from "vue-chartjs";
 
-ChartJS.register(CategoryScale, LinearScale, BarElement, Title, Tooltip, Legend)
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  BarElement,
+  Title,
+  Tooltip,
+  Legend
+);
 
 export default {
-  name: 'App',
+  name: "App",
   components: {
-    Bar
+    Bar, Bar
   },
   data() {
     return {
       showGraph: false,
       data: {
-        labels: ['Black', 'Gray', 'Cinnamon'],
-        datasets: [{ data: [] }]
+        labels: [ "Black", "Gray", "Cinnamon"], 
+     
+        datasets: [{ data: [] }],
       },
       options: {
         responsive: true,
-        backgroundColor: ['#000000', '#808080', '#D2691E']
-      }
-    }
+        backgroundColor: ["#000000", "#808080", "#D2691E"],
+      },
+      showGraphs: false,
+      data2: {
+        labels: [ "Adult", "Juvenile"], 
+     
+        datasets: [{ data: [] }],
+      },
+     
+
+   
+    };
+    
   },
-  async mounted() {
-    let res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
-    let dataYep = await res.json()
-    console.log(dataYep)
-    const black = dataYep.filter((squirrelYes) => squirrelYes.primary_fur_color === 'Black')
-    this.data.datasets[0].data.push(black.length)
-    console.log(black.length)
-    const gray = dataYep.filter((squirrelYes) => squirrelYes.primary_fur_color === 'Gray')
-    this.data.datasets[0].data.push(gray.length)
-    console.log(dataYep)
-    console.log(gray.length)
-    console.log(dataYep)
+  methods: {
+    async createGraph() {
+      let res = await fetch(
+        "https://data.cityofnewyork.us/resource/vfnx-vebw.json"
+      );
+      let dataYep = await res.json();
+      console.log(dataYep);
+      let inputxd = document.querySelector("#input").value;
 
-    const cinnamon = dataYep.filter((squirrelYes) => squirrelYes.primary_fur_color === 'Cinnamon')
-    this.data.datasets[0].data.push(cinnamon.length)
-    console.log(cinnamon.length)
-  }
-}
+      console.log(inputxd);
+      if (inputxd === "Primary Fur Color") {
+        console.log("hi");
+        this.showGraph = true
+        this.showGraphs = false
+
+        const black = dataYep.filter(
+          (squirrelYes) => squirrelYes.primary_fur_color === "Black"
+        );
+        this.data.datasets[0].data.push(black.length);
+
+        const gray = dataYep.filter(
+          (squirrelYes) => squirrelYes.primary_fur_color === "Gray"
+        );
+        this.data.datasets[0].data.push(gray.length);
+
+        const cinnamon = dataYep.filter(
+          (squirrelYes) => squirrelYes.primary_fur_color === "Cinnamon"
+        );
+        this.data.datasets[0].data.push(cinnamon.length);
+      } else if (inputxd === "Age") {
+        console.log("bye");
+        this.showGraph = false
+        this.showGraphs = true
+        const Adult = dataYep.filter(
+          (squirrelYes) => squirrelYes.age === "Adult"
+        );
+        this.data2.datasets[0].data.push(Adult.length);
+        const Juvenile = dataYep.filter(
+          (squirrelYes) => squirrelYes.age === "Juvenile"
+        );
+        this.data2.datasets[0].data.push(Juvenile.length);
+      }
+    },
+  },
+};
 </script>
-
-<style scoped>
-#routing {
-  display: block;
-}
-#button {
-  display: flex;
-  flex-direction: column;
-}
-</style>
