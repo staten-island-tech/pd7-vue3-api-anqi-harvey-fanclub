60cbd07caa0fd29e7e6fd44437ac64732e5a7109
YifanZhang4 <bookworm.with.a.problem@gmail.com>
2023-05-02 12:55:18 -0400

    l

--- open-api/src/components/DoughnutChart.vue
+++ open-api/src/components/DoughnutChart.vue
@@ -1,10 +1,8 @@
 <template>
   <div class="chart">
-    <RouterLink to="/">Back to Main</RouterLink>
-    <br />
-    <RouterLink to="/bar">To Bar</RouterLink>
-    <h2>Squirrel Primary Colors - Doughnut Chart</h2>
     <Doughnut v-if="loaded" :data="chartData" :options="chartOptions" />
+    <button @click="getColor()">See By Color</button>
+    <button @click="getAge()">See By Age</button>
   </div>
 </template>
 
@@ -22,49 +20,69 @@ export default {
   data() {
     return {
       loaded: false,
+      squirrelColor: false,
+      squirrelAge: false,
       chartData: {
-        labels: ['Gray', 'Cinnamon', 'Black'],
+        labels: [],
         datasets: [{ data: [] }]
       },
       chartOptions: {
         responsive: true,
-        maintainAspectRatio: true,
-        backgroundColor: ['#7B888E', '#9c4722', '#282d30']
+        backgroundColor: []
       }
     }
   },
-  async mounted() {
-    try {
-      const res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
-      const squirrelData = await res.json()
-      const gray = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Gray')
-      this.chartData.datasets[0].data.push(gray.length)
+  methods: {
+    async getColor() {
+      this.squirrelColor = true
+      this.squirrelAge = false
+      this.loaded = false
+      try {
+        const res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+        const squirrelData = await res.json()
+        const gray = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Gray')
 
-      const cinnamon = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Cinnamon')
-      this.chartData.datasets[0].data.push(cinnamon.length)
+        const cinnamon = squirrelData.filter(
+          (squirrel) => squirrel.primary_fur_color === 'Cinnamon'
+        )
 
-      const black = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Black')
-      this.chartData.datasets[0].data.push(black.length)
+        const black = squirrelData.filter((squirrel) => squirrel.primary_fur_color === 'Black')
+        this.chartData.datasets[0].data = [gray.length, cinnamon.length, black.length]
 
-      this.loaded = true
-    } catch (e) {
-      console.error(e)
+        this.chartData.labels = ['Gray', 'Cinnamon', 'Black']
+
+        this.chartOptions.backgroundColor = ['#7B888E', '#9c4722', '#3b444b']
+
+        this.loaded = true
+      } catch (e) {
+        console.error(e)
+      }
+      console.log(this.chartData)
+    },
+    async getAge() {
+      this.squirrelColor = false
+      this.squirrelAge = true
+      this.loaded = false
+      try {
+        const res = await fetch('https://data.cityofnewyork.us/resource/vfnx-vebw.json')
+        const squirrelData = await res.json()
+        const adult = squirrelData.filter((squirrel) => squirrel.age === 'Adult')
+
+        const juvenile = squirrelData.filter(
+          (squirrel) => squirrel.primary_fur_color === 'Juvenile'
+        )
+        this.chartData.datasets[0].data = [adult.length, juvenile.length]
+
+        this.loaded = true
+
+        this.chartData.labels = ['Adult', 'Juvenile']
+
+        this.chartOptions.backgroundColor = ['#b20019', '#272a2b']
+      } catch (e) {
+        console.error(e)
+      }
+      console.log(this.chartData)
     }
-    console.log(this.chartData)
   }
 }
 </script>
-
-<style scoped>
-h2 {
-  color: #7a6e63;
-  font-size: 2rem;
-  text-align: center;
-}
-h2:hover {
-  text-decoration: underline;
-  transition: 1s;
-  color: #7a6e63;
-  font-size: 2.5rem;
-}
-</style>
